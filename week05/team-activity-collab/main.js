12f6e905ba754b792fec39f59040915b259b9457
ellenhc <how19034@byui.edu>
2020-05-21 12:50:32 -0700

    added team activity collab files

--- week05/team-activity-collab/main.js
+++ week05/team-activity-collab/main.js
@@ -1,5 +1,9 @@
 //Create a main.js and add that to your HTML with your script element
 
 //You need to import your module before you can use it. Import hikes from ./hikes.js
+import Hikes from './modules/hikes.js';
 
-//You need to create an instance of the Hike class before you can use it. const myHike = new Hikes('hikeListId');
+//You need to create an instance of the Hike class before you can use it. 
+const myHike = new Hikes('hikes');
+
+myHike.showHikeList();
