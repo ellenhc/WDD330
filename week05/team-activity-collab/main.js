e7e2063d63fd57d20073f79762e3fdb98cbb343e
ellenhc <59638391+ellenhc@users.noreply.github.com>
2020-05-20 23:42:15 -0700

    working on team activity

---
+++ week05/team-activity-collab/main.js
@@ -0,0 +1,5 @@
+//Create a main.js and add that to your HTML with your script element
+
+//You need to import your module before you can use it. Import hikes from ./hikes.js
+
+//You need to create an instance of the Hike class before you can use it. const myHike = new Hikes('hikeListId');
