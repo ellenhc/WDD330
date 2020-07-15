2c919c49ec970c3f93639f4b8d03ea8de45e12b1
ellenhc <59638391+ellenhc@users.noreply.github.com>
2020-07-14 20:35:43 -0700

    continued work on final project

--- Project/js/main.js
+++ Project/js/main.js
@@ -7,6 +7,9 @@ const startURL = "https://opentdb.com/api.php?";
 
 //the url is a json file. each multiple choice question has 4 answers. each question has a category, type, difficulty, question, correct_answer, and incorrect_answers
 
+let questionList = [];
+let currentQuestionIndex = 0;
+
 const form = document.querySelector("form"); //grabs the form element
 
 form.addEventListener('submit', (event) => {
@@ -18,13 +21,82 @@ form.addEventListener('submit', (event) => {
         mode: 'cors',
     })
     fetch(request)
-        .then(response => response.json()) // convert from json to object
+        .then(response => response.json()) // converts from json to object
         .then(data => data.results) // then grab just the results
         .then(storeQuestions)
         .catch(error => console.log('There was an error:', error))
 });
 
 function storeQuestions(questions) {
-    console.log(questions);
+    for (let i = 0; i < questions.length; i++) {
+        let question = { question: questions[i].question, correct_answer: questions[i].correct_answer, incorrect_answers: questions[i].incorrect_answers };
+        questionList.push(question);
+    }
+    //console.log(questions);
+    console.log(questionList);
+    renderOneQuestion();
+}
 
+function getQuestionList() {
+    return questionList;
+}
+
+function getCurrentQuestion() {
+    return questionList[currentQuestionIndex];
+}
+
+function renderOneQuestion() {
+    //step 1 - remove page content (this works, i've checked)
+    let node = document.querySelector("body");
+    node.innerHTML = "";
+    //step 2 - render to page
+    const currentQuestion = getCurrentQuestion();
+    const container = document.createElement("div");
+    container.classList.add("trivia-container"); //adds a class
+
+    const questionDiv = document.createElement("div");
+    questionDiv.classList.add("question-div"); //adds a class to style the question
+    questionDiv.innerHTML = `<h1>${currentQuestion.question}</h1>`;
+    node.appendChild(container);
+    container.appendChild(questionDiv);
+
+    const questionContainer = document.createElement("div");
+    questionContainer.classList.add("question-container");
+
+    let randomAnswer = [0, 1, 2, 3]; //has same length as number of possible answers
+    //Shuffles array to get a number in no particular order using the Fisher Yates method
+    for (i = randomAnswer.length - 1; i > 0; i--) {
+        j = Math.floor(Math.random() * i)
+        k = randomAnswer[i]
+        randomAnswer[i] = randomAnswer[j]
+        randomAnswer[j] = k
+    }
+    console.log(randomAnswer);
+    randomAnswer.forEach((i) => {
+        if (i == 3) {
+            const answer = document.createElement("div");
+            answer.innerHTML = `<p>${currentQuestion.correct_answer}</p>`;
+            answer.classList.add("answer");
+            questionContainer.appendChild(answer);
+
+            //add a click handler for correct answer
+            answer.addEventListener("click", () => {
+                //change background-color to green
+                answer.style.backgroundColor = "#007849";
+            })
+        } else {
+            const answer = document.createElement("div");
+            answer.innerHTML = `<p>${currentQuestion.incorrect_answers[i]}</p>`;
+            answer.classList.add("answer");
+            questionContainer.appendChild(answer);
+
+            //add a click handler for incorrect answers
+            answer.addEventListener("click", () => {
+                //change background-color to red
+                answer.style.backgroundColor = "#C30916";
+            })
+        }
+    });
+
+    container.appendChild(questionContainer); //adds the question container
 }
