0b5eb25f8be1f30e572be2409d47034c22f77ef1
ellenhc <59638391+ellenhc@users.noreply.github.com>
2020-07-16 16:40:34 -0700

    added a score tracker, now user can only select 1 answer

--- Project/js/main.js
+++ Project/js/main.js
@@ -2,6 +2,7 @@ const startURL = "https://opentdb.com/api.php?";
 
 let questionList = [];
 let currentQuestionIndex = 0;
+let score = 0;
 
 const form = document.querySelector("form"); //grabs the form element
 
@@ -25,8 +26,7 @@ function storeQuestions(questions) {
         let question = { question: questions[i].question, correct_answer: questions[i].correct_answer, incorrect_answers: questions[i].incorrect_answers }; //just saves the question, the correct answer, and the incorrect answers and not the other data
         questionList.push(question); //adds it to the questionList array
     }
-    //console.log(questions);
-    //console.log(questionList);
+
     renderOneQuestion();
 }
 
@@ -42,6 +42,7 @@ function renderOneQuestion() {
     /*step 1 - remove previous page content*/
     const node = document.querySelector("body");
     node.innerHTML = "";
+    renderStats(node);
 
     /*step 2 - renders current question to page*/
     const currentQuestion = getCurrentQuestion();
@@ -55,7 +56,7 @@ function renderOneQuestion() {
     container.appendChild(questionDiv); //adds questionDiv to page
 
     const answerContainer = document.createElement("div"); //creates container to hold the answers
-    answerContainer.classList.add("question-container"); //adds a class to that container
+    answerContainer.classList.add("answer-container"); //adds a class to that container
 
     let randomAnswer = [0, 1, 2, 3]; //has same length as number of possible answers
     //Shuffles array to get a number in no particular order using the Fisher Yates method
@@ -74,9 +75,14 @@ function renderOneQuestion() {
 
             //add a click handler for correct answer
             answer.addEventListener("click", () => {
-                //change background-color to green
-                answer.style.backgroundColor = "#007849";
-                answer.style.transition = "all 0.5s"; //adds gradual transition to green color
+                if (!container.classList.contains("done")) {
+                    container.classList.add("done"); //adds class "done"
+
+                    answer.style.backgroundColor = "#007849"; //change background-color to green
+                    answer.style.transition = "all 0.5s"; //adds gradual transition to green color
+
+                    score++;
+                }
             })
         } else {
             const answer = document.createElement("div");
@@ -86,9 +92,11 @@ function renderOneQuestion() {
 
             //add a click handler for incorrect answers
             answer.addEventListener("click", () => {
-                //change background-color to red
-                answer.style.backgroundColor = "#C30916";
-                answer.style.transition = "all 0.5s"; //adds a gradual transition to red color
+                if (!container.classList.contains("done")) {
+                    container.classList.add("done"); //adds class "done"
+                    answer.style.backgroundColor = "#C30916"; //change background-color to red
+                    answer.style.transition = "all 0.5s"; //adds a gradual transition to red color
+                }
             })
         }
     });
@@ -96,12 +104,6 @@ function renderOneQuestion() {
     container.appendChild(answerContainer); //adds the answer container to page
     renderButtons();
 }
-
-/*function renderAllQuestions() {
-    for (let i = 0; i < questionList.length; i++) {
-        renderOneQuestion();
-    }
-}*/
 
 function renderButtons() {
     const node = document.querySelector(".trivia-container");
@@ -124,4 +126,12 @@ function renderButtons() {
     })
 
     node.appendChild(buttonContainer);
+}
+
+function renderStats(node) {
+    const statsBox = document.createElement("div");
+    statsBox.classList.add("stats-box");
+    node.appendChild(statsBox);
+
+    statsBox.innerHTML = `<div class="score-box"><p>Score:</p> <h3>${score} / ${questionList.length}</h3></div>`;
 }
